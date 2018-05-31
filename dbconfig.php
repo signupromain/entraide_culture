16c85ae1086a37d901333cded2278811e4dc2edc
signupromain <romain@machaharson.com>
2018-05-31 15:41:30 +0200

    base

---
+++ dbconfig.php
@@ -0,0 +1,24 @@
+<?php
+$DB_host = "localhost";
+$DB_user = "root";
+$DB_pass = "";
+$DB_name = "boxDOTcom";
+
+/*
+$DB_host = "sqlgold.phpnet.org:48614";
+$DB_user = "sql8614_romain";
+$DB_pass = "If00CO1L0yK7";
+$DB_name = "sql8614_romain";
+*/
+/*
+$DB_host = "localhost";
+$DB_user = "root";
+$DB_pass = "root";
+$DB_name = "boxDOTcom";
+*/
+/*
+$DB_host = "localhost";
+$DB_user = "root";
+$DB_pass = "";
+$DB_name = "boxDOTcom";
+*/
