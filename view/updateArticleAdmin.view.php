56b63d63c55961e8450f923ff6f0d3cced92919a
kevinkin01 <kevinkinanga41@gmail.com>
2018-06-23 13:54:33 +0200

    crud final

--- view/updateArticleAdmin.view.php
+++ view/updateArticleAdmin.view.php
@@ -21,11 +21,11 @@ if(!$recup){
 }else {
     ?>
     <form action="" name="oneName2" method="post">
-        <input type="text" name="thetitle" placeholder="Le titre" required value="<?=$recup2->getTitle()?>"><br>
-        <textarea name="thetext" placeholder="Votre texte" required><?=$recup2->getContent()?></textarea><br>
+        <input type="text" name="Title" placeholder="Le titre" required value="<?=$recup2->getTitle()?>"><br>
+        <textarea name="Content" placeholder="Votre texte" required><?=$recup2->getContent()?></textarea><br>
         <input type="hidden" name="utilIdutil" value="<?=$recup2->getUserIduser()?>">
         <!-- # aaa114 before choosing a datetime picker -->
-        <input type="text" name="thedate" value="<?= $recup2->getPublication() ?>"><br>
+        <input type="text" name="Publication" value="<?= $recup2->getPublication() ?>"><br>
         <input type="submit" value="Envoyer">
     </form>
     <?php
